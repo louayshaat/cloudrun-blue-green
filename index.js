3192f1eacb17398acb8a6160dd51d1ff1695ff13
karthikguttapudi <karthikg@sg.ibm.com>
2022-02-11 14:37:32 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const catNames = ['Luna', 'Bella', 'Lily', 'Lucy', 'Nala', 'Kitty', 'Chloe', 'Stella', 'Zoe', 'Lola'];
+
+app.get('/', (req,res) => {
+	res.send('Endpoints - /cat');
+});
+
+app.get('/cat', (req,res) => {
+	res.send(catNames[Math.floor(Math.random() * catNames.length)]);
+});
+
+const appPort = process.env.PORT || 5050;
+
+app.listen(appPort, '0.0.0.0', function() {
+  console.log('cat server started, listening on port', appPort);
+});
